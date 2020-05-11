2fa8da5606921d00a941e65d1f50f17f6f7cb2c7
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-05-11 21:06:20 +0700

    Share by RedT

---
+++ Helpers.java
@@ -0,0 +1,132 @@
+package RedT;
+
+// API ZingMp3
+// Code by RedT
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.SignatureException;
+import java.util.*;
+
+import java.net.URLEncoder;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Helpers {
+	public static final String MAIN_URL    = "https://zingmp3.vn";
+	public static final String XHR_URL     = "https://m.zingmp3.vn/xhr";
+	public static final String API_V5_URL  = "https://m.zingmp3.vn/api";
+	public static final String MP3_STATIC  = "https://static-zmp3.zadn.vn/skins/zmp3-mobile-v5.2";
+	public static final String PHOTO_URL   = "https://photo-zmp3.zadn.vn/";
+
+	private static final String URL_DETAIL       = "/chart-realtime/get-detail";
+	private static final String URL_SOUNG_INFO   = "/song/get-song-info";
+	private static final String URL_LYRIC        = "/song/get-lyric";
+	private static final String URL_STREAMMING   = "/song/get-streamings";
+	private static final String URL_PLAYLIST     = "/playlist/get-playlist-detail";
+	private static final String URL_SEARCH       = "/api/search";
+	private static final String URL_SEARCH_MULTI = "/search/multi";
+
+	public static final String REGEX_URL    = "(https?:)?\\/\\/(.*)";
+
+	private static final String HMAC_SHA512 = "HmacSHA512";
+	public static final String API_KEY      = "38e8643fb0dc04e8d65b99994d3dafff";
+	public static final String PRIVATE_KEY  = "10a01dcf33762d3a204cb96429918ff6";
+	public static int ctime   = 1589168845;
+	public static String type = "song";
+	public  static String sig = "";
+
+	// Sort by Key HashMap
+	public static Map<String, String> sortByKey(Map<String, String> map) {
+		TreeMap<String, String> sorted = new TreeMap<>(map);
+		return sorted;
+	}
+
+	// SHA 256
+	public static String toHexStringSHA256(byte[] hash) {
+		BigInteger number = new BigInteger(1, hash);
+		StringBuilder hexString = new StringBuilder(number.toString(16));
+		while (hexString.length() < 32) {
+			hexString.insert(0, '0');
+		}
+		return hexString.toString();
+	}
+	public static String getSHA256(String input) throws NoSuchAlgorithmException
+	{
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		return toHexStringSHA256(md.digest(input.getBytes(StandardCharsets.UTF_8)));
+	}
+
+	// MAC SHA512
+	private static String toHexStringMAC512(byte[] bytes) {
+		Formatter formatter = new Formatter();
+		for (byte b : bytes) {
+			formatter.format("%02x", b);
+		}
+		return formatter.toString();
+	}
+	public static String calculateHMAC(String data, String key) throws SignatureException, NoSuchAlgorithmException, InvalidKeyException {
+		SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), HMAC_SHA512);
+		Mac mac = Mac.getInstance(HMAC_SHA512);
+		mac.init(secretKeySpec);
+		return toHexStringMAC512(mac.doFinal(data.getBytes()));
+	}
+
+	public static String getCurrentTime(){
+		Date date = new Date();
+		return String.valueOf(date.getTime()/1000);
+	}
+
+	// BUILD URL
+	public static String buildAPIURL(String api, Map<String, String> param) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+		param.put("ctime", getCurrentTime());
+		param.put("sig", buildSig(api, param));
+		return API_V5_URL + api + "?" + buildQuery(param, "&") + "api_key=" + API_KEY;
+	}
+
+	public static String buildSig(String api, Map<String, String> param) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+		String n = buildMessage(param);
+		sig = calculateHMAC(api + getSHA256(n), PRIVATE_KEY);
+		return sig;
+	}
+
+	// buildQuery
+	public static String buildQuery(Map<String, String> param, String sq){
+		String cer = "";
+		for (String key : param.keySet()) {
+			cer += key + "=" + URLEncoder.encode(param.get(key)) + sq;
+		}
+		return cer;
+	}
+
+	public static String buildMessage(Map<String, String> param){
+		param = sortByKey(param);
+
+		Map<String, String> newCer = new HashMap<String, String>();
+		for (String key : param.keySet()) {
+			if ((key.equals("ctime") || key.equals("id")) && null != param.get(key)) {
+				newCer.put(key, param.get(key));
+			}
+		}
+		return buildQuery(newCer, "");
+	}
+
+	// add https to url
+	public static String patternURL(String url){
+		Pattern pattern = Pattern.compile(REGEX_URL);
+		Matcher matcher = pattern.matcher(url);
+		if(matcher.find()) {
+			url = matcher.group(2);
+			if(!url.isEmpty()){
+				url = "https://" + url;
+			}
+		}
+		return url;
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,62 @@
+
+
+API Zing MP3 | RedT
+
+
+Code triển khai các api lấy thông tin nhạc của zing mp3 như lấy thông tin: bài hát, link tải, playlist, zingchart...
+
+// Ví dị lấy 100 bài hát thịnh hành hiện tại
+
+ Map<String, String> paramChart = new HashMap<String, String>();
+ paramChart.put("type", "song");
+ paramChart.put("time", "-1");
+ paramChart.put("count", "100");
+
+ String chartRealtime    = Helpers.buildAPIURL(Helpers.URL_DETAIL, paramChart);
+ System.out.println(chartRealtime);
+
+
+ //Lấy thông tin bài hát, link tải, ảnh bìa,... của bài hát cho trước
+ //For example, get song information
+ Map<String, String> soungInfoParam = new HashMap<String, String>();
+ soungInfoParam.put("id", "ZWBWAIDA");
+
+ String soungInfo = Helpers.buildAPIURL(Helpers.URL_SOUNG_INFO, soungInfoParam);
+ System.out.println(soungInfo);
+
+
+
+// Các API khác, 
+    // API
+
+	// get Zing Chart
+    // URL   /chart-realtime/get-detail
+    // param type=song&time=-1&count=100
+
+	// get Soung info
+    // URL   /song/get-song-info
+    // param id=XXXXXXX
+
+	// get Lyric
+    // URL   /song/get-lyric
+    // param id=XXXXXXX
+
+	// get Streamings
+	// URL   /song/get-streamings
+    // param id=XXXXXXX
+
+	// get Playlist
+    // URL   /playlist/get-playlist-detail
+    // param id=XXXXXXX
+
+	// get search
+    // URL   /api/search
+    // param type=song&q=??????&start=0&count=20
+
+	// get search multi
+    // URL   /search/multi
+    // param q=???????
+
+
+![Code Test](https://github.com/toannguyen2/zingmp3_api/demo.png)
+![API trả về json](https://github.com/toannguyen2/zingmp3_api/json.png)
